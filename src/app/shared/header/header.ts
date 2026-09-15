a4a64e508b5e1474dd658234489d7d1bc3eff3a1
Edinson Hernandez Cubillos <ehernandezc@poligran.edu.co>
2026-09-15 12:07:25 -0500

    chages.css

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // 1. Agregamos la importación
 
 @Component({
-  imports: [],
   selector: 'app-header',
-  styleUrl: './header.css',
+  imports: [RouterLink], // 2. Lo declaramos aquí para que funcione en el HTML
   templateUrl: './header.html',
+  styleUrl: './header.css'
 })
 export class HeaderComponent {}
