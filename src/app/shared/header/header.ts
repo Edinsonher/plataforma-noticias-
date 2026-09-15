52f5eef448637a145452f989985d4891ef3b9d54
Edinson Hernandez Cubillos <ehernandezc@poligran.edu.co>
2026-09-15 12:38:26 -0500

    Modificado

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -3,8 +3,10 @@ import { RouterLink } from '@angular/router'; // 1. Agregamos la importación
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink], // 2. Lo declaramos aquí para que funcione en el HTML
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
+
 })
 export class HeaderComponent {}
