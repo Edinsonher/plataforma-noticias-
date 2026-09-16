b6981931384a556c70f2d72878186ddcc9ea4a2c
Edinson Hernandez Cubillos <ehernandezc@poligran.edu.co>
2026-09-16 17:42:41 -0500

    se agrega el archivo json

--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [],
   selector: 'app-noticias',
+  imports: [CommonModule],
   styleUrl: './noticias.css',
-  templateUrl: './noticias.html',
+  templateUrl: './noticias.html'
 })
-export class NoticiasComponent {}
+export class NoticiasComponent implements OnInit {
+  // Arreglo donde se guardarán las noticias cargadas del JSON
+  listaNoticias: any[] = [];
+
+  // Inyectamos el HttpClient en el constructor
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Leemos el archivo JSON que creamos en la carpeta public
+    this.http.get<any[]>('noticias.json').subscribe({
+      next: (data) => {
+        this.listaNoticias = data;
+        console.log('Noticias cargadas con éxito:', this.listaNoticias);
+      },
+      error: (err) => {
+        console.error('Error al cargar el archivo JSON:', err);
+      }
+    });
+  }
+}
+
